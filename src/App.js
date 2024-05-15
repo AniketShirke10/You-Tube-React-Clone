7d12b032eb85e4af741163ed5cedd8749e280fd4
AniketShirke10 <111981218+AniketShirke10@users.noreply.github.com>
2024-05-15 11:00:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div> You_Tube_clone </div>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDom from 'react-dom'
+import React from 'react'
+import App from "./App"
+
+ReactDOM.render(<App />, Document.getElementById("root")) 
